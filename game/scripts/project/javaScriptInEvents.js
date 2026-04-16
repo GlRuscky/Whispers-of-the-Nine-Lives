ee7a564a5443667bf94392da63c9f949e84a6260
GlRuscky <149537222+GlRuscky@users.noreply.github.com>
2026-04-16 18:47:49 -0300

    Add files via upload

---
+++ game/scripts/project/javaScriptInEvents.js
@@ -0,0 +1,12 @@
+
+
+const scriptsInEvents = {
+
+	async FolhaDeEventos1_Event23(runtime, localVars)
+	{
+		console.log("Jogo iniciado com sucesso!");
+		
+	}
+};
+
+globalThis.C3.JavaScriptInEvents = scriptsInEvents;
